0f241a498eeac350ab61608017b9564abdabce31
juliovt-07 <jcproducoes.verytop@gmail.com>
2020-05-21 00:51:51 -0300

    API de Clima

---
+++ requestTest.js
@@ -0,0 +1,8 @@
+var request = require('request');
+request('http://api.openweathermap.org/data/2.5/weather?q=Imperatriz,br&appid=a1b3de34cb3e70cfcc791b096b2ff862', function (error, response, body) {
+//  console.log('error:', error); // Print the error if one occurred
+//  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//  console.log('body:', body); // Print the HTML for the Google homepage.
+  var parsedWeather = JSON.parse(body);
+  console.log(parsedWeather)
+});
